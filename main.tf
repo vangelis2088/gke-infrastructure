5efebe74ede52c7ef747043f935cd7d1629987b6
Evangelos Tsougiannis <evangelos.tsougiannis@cibos.gr>
2023-03-07 13:47:47 +0200

    Initial commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+module "dev_cluster" {
+    source     = "./main"
+    env_name   = "dev"
+    project_id = "${var.project_id}"
+}
+
+module "staging_cluster" {
+    source     = "./main"
+    env_name   = "staging"
+    project_id = "${var.project_id}"
+}
+
+module "prod_cluster" {
+    source     = "./main"
+    env_name   = "prod"
+    project_id = "${var.project_id}"
+}
+
+variable "project_id" {
+  description = "The project ID to host the cluster in"
+  default = "gifted-cooler-378519"
+}
